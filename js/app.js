ec2b45d8df560789209782d0d0c8d6a9e926e1c5
Varun Singh <55821761+Iamvarunking@users.noreply.github.com>
2021-07-03 20:09:33 +0530

    added mobile nav

--- js/app.js
+++ js/app.js
@@ -6,5 +6,20 @@ $(document).ready(function() {
         dotsClass:'dots'
 
 
-    })
+    });
+
+   
+    
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+
+    hamberger.addEventListener('click' , ()=>{
+        mobileNav.classList.add('open');
+    });
+
+    times.addEventListener('click' , ()=>{
+        mobileNav.classList.remove('open');
+
+    });
 })
